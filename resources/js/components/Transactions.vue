884a4e3c0d3e4bcda2d0dfedb3ba5afa0fadb393
Chris Shaw <me@cshaw.xyz>
2020-08-07 00:46:48 +1000

    Minor round up tweak in transactions

--- resources/js/components/Transactions.vue
+++ resources/js/components/Transactions.vue
@@ -72,7 +72,7 @@
               <path class="text-up" fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clip-rule="evenodd"></path>
             </svg>
 
-            {{ round_up.amount.value | formatAmount }}
+            +{{ round_up.amount.value | absoluteValue | formatAmount }}
           </div>
         </div>
 
@@ -105,6 +105,10 @@ export default {
     addAmounts(a, b) {
       return parseFloat(a) + parseFloat(b)
     },
+  },
+
+  filters: {
+    absoluteValue: (value) => Math.abs(parseFloat(value)),
   },
 }
 </script>
